2d94d41e7faae4049aba9203dc5d8061c6d0d22d
chscz <chs106@naver.com>
2022-07-25 19:14:23 +0900

    커밋1

---
+++ calc1/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+from django.http import JsonResponse
+# Create your views here.
+
+def index(request):
+    print("1111111")
+    return render(request, 'index.html')
+
+def test1(request):
+    print("222222")
+    return render(request, 'index.html')
+
